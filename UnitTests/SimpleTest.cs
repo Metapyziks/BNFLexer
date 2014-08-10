50d83410b14fa62dd77697abf6cb44122275fdf3
Metapyziks <metapyziks@gmail.com>
2014-08-10 15:44:28 +0100

    Fixed some mistakes in simple.ebnf

---
+++ UnitTests/SimpleTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using BNFLexer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SimpleTest
+    {
+        [TestMethod]
+        public void SimpleGrammarTest1()
+        {
+            var src = File.ReadAllText("Resources/grammars/simple.ebnf");
+            var ruleset = Ruleset.FromString(src);
+
+            return;
+        }
+    }
+}
